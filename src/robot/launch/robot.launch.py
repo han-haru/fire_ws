80dda99378ee6b0b6d7389f8fb46e8e84962abf5
han_haru <164311866+han-haru@users.noreply.github.com>
2025-09-09 01:41:50 +0900

    Update robot.launch.py

--- src/robot/launch/robot.launch.py
+++ src/robot/launch/robot.launch.py
@@ -7,7 +7,8 @@ def generate_launch_description():
             package='robot',
             executable='blade_node',
             name='blade_node',
-            output='screen'
+            output='screen',
+            parameters= [{'pwm_active_high': True}]
         ),
         Node(
             package='robot',
@@ -21,13 +22,13 @@ def generate_launch_description():
             name='pump_node',
             output='screen'
         ),
-        Node(
-            package='robot',
-            executable='teleop_node',
-            name='teleop_node',
-            output='screen',
-            emulate_tty=True
-        ),
+        # Node(
+        #     package='robot',
+        #     executable='teleop_node',
+        #     name='teleop_node',
+        #     output='screen',
+        #     emulate_tty=True
+        # ),
         Node(
             package='camera_ros',
             executable='camera_node',
@@ -38,8 +39,21 @@ def generate_launch_description():
                 "format": "BGR888",
                 "width": 640, "height": 480, "fps": 15,
                 "image_transport": "compressed"},
-                'config/camera_compressed.yaml'    # ← 이 줄 추가
+                'config/camera_compressed.yaml'   
             ],
+            output='screen'
+        ),
+        Node(
+            package='robot',
+            executable='auto_flip',
+            name='auto_flip',
+            parameters=[{
+                'imu_topic': '/imu/data',  # 실제 IMU 토픽명으로 맞추세요
+                'in_topic':  '/imx219_cam/image_raw/compressed',
+                'out_topic': '/imx219_cam/image_flipped/compressed',
+                'invert_threshold_deg': 120.0,  # 이 이상 기울면 뒤집기 ON
+                'recover_threshold_deg': 60.0   # 이 이하로 돌아오면 뒤집기 OFF (히스테리시스)
+            }],
             output='screen'
         ),
 
